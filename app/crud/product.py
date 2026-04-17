581a7c076e1ffeb404019e302d744619de724bae
Faith-tech-code <akirorfaith45@gmail.com>
2026-04-17 17:19:37 +0300

    feat: add user CRUD with email lookup and password hashing feat: add product CRUD with vendor filtering

--- app/crud/product.py
+++ app/crud/product.py
@@ -1,4 +1,11 @@
-from app.crud.base import CRUDBase
+from sqlalchemy.orm import Session
 from app.models.product import Product
+from app.crud.base import CRUD
 
-product = CRUDBase(Product)
+
+class CRUDProduct(CRUD):
+    def get_by_vendor(self, db: Session, vendor_id: int):
+        return db.query(Product).filter(Product.vendor_id == vendor_id).all()
+
+
+product_crud = CRUDProduct(Product)
--- app/crud/user.py
+++ app/crud/user.py
@@ -1,4 +1,16 @@
-from app.crud.base import CRUDBase
+from sqlalchemy.orm import Session
 from app.models.user import User
+from app.core.security import hash_password
+from app.crud.base import CRUD
 
-user = CRUDBase(User)
+
+class CRUDUser(CRUD):
+    def get_by_email(self, db: Session, email: str):
+        return db.query(User).filter(User.email == email).first()
+
+    def create(self, db: Session, data: dict):
+        data["password"] = hash_password(data["password"])
+        return super().create(db, data)
+
+
+user_crud = CRUDUser(User)
