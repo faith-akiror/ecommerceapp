1120c23df4b9b479ef4ae6d5c484d7af383ca451
Faith-tech-code <akirorfaith45@gmail.com>
2026-04-17 15:56:55 +0300

    feat: add product model with vendor relationship

---
+++ app/models/product.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String, Float, ForeignKey
+from app.core.database import Base
+
+class Product(Base):
+    __tablename__ = "products"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    price = Column(Float)
+    stock = Column(Integer)
+    vendor_id = Column(Integer, ForeignKey("users.id"))
